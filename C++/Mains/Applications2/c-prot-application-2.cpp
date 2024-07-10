fa846ebf85d63d3abdab15eb6c7aecfd7b0a459a
Arda Doğrul <darda159@gmail.com>
2024-07-10 17:33:26 +0300

    Update Applications2

--- C++/Mains/Applications2/c-prot-application-2.cpp
+++ C++/Mains/Applications2/c-prot-application-2.cpp
@@ -12,20 +12,24 @@ int main(){
     cout<<"Dosya Diznini Giriniz :\n ";
     cin>>_directory;
     char *arr2 = ReadAllBytes(_directory);
-    streamoff fileSize = sizeof(arr2);
-
+    
+    ifstream file(_directory, ios::out | ios::binary);
+    file.seekg(0, ios::end); 
+	int fileSize = file.tellg();                  //Aynı kodu header'da kullandık dosya boyutunu veriyor
+    file.seekg(0, ios::beg);                            //(EN) Get the size of the array
+                                
+    cout<<fileSize<<endl;                                          
     for (int i = 0; i < fileSize; i++)
     {
-        char temp[8]  = {arr2[i], arr2[i+1], arr2[i+2], arr2[i+3], arr2[i+4], arr2[i+5], arr2[i+6]};
+        char temp[8]  = {arr2[i], arr2[i+1], arr2[i+2], arr2[i+3], arr2[i+4], arr2[i+5], arr2[i+6]};    //(TR) Dizinin her 8 elemanını al     
+                                                                                                        //(EN) Get the every 8 value of array
         strncpy(temp, temp, 8);
         if(strcmp(temp, arr) == 0){
-            cout<<"Malware bulundu\n";
+            cout<<"\"MALWARE\" ifadesi "<<i<<" : ("<<(void*)(arr2 + i)<<") adresinde bulundu\n";    //(TR)cout işaretçileri void olarak yazdırabildiği için bu şşekilde adresini de yazdık offset değerini de
+                                                                                     //(EN) We wrote the address of the "MALWARE" expression in this way because cout can print pointers as void
             break;
         }
-        else{
-            cout<<"Malware bulunamadı\n";
-            break;
-        }
+                                
     }
     
 }
